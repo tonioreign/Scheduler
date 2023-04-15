cb3aaa67f795f0b6a0740ce10be1e643ad90a636
tonio <tonioreign@gmail.com>
2023-04-15 15:20:45 -0700

    REFACTORING & JAVADOCS

---
+++ JavaDocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sample.controllers"},{"l":"sample.db"},{"l":"sample.main"},{"l":"sample.misc"},{"l":"sample.models"},{"l":"sample.utils"}];updateSearchResults();
